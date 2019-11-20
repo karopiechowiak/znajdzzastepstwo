f25d49597044e3cc73f223d3cf0279019d99e2aa
Karolina Piechowiak <karolinapiechowiak@MacBook-Air-Karolina.local>
2019-11-20 16:55:06 +0100

    fixed firebase problem

--- src/js/index.js
+++ src/js/index.js
@@ -6,24 +6,27 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../store/reducers/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import { createFirestoreInstance } from "redux-firestore";
-import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
 import firebaseConfig from "./../firebase/firebaseConfig";
 
-const store = createStore(rootReducer, compose());
-
-const rrfProps = {
-  firebase,
-  config: firebaseConfig,
-  dispatch: store.dispatch,
-  createFirestoreInstance
-};
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(
+      thunk.withExtraArgument({
+        getFirebase,
+        getFirestore
+      })
+    ),
+    reduxFirestore(firebaseConfig),
+    reactReduxFirebase(firebaseConfig)
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
-    <ReactReduxFirebaseProvider>
-      <App />
-    </ReactReduxFirebaseProvider>
+    <App />
   </Provider>,
   document.getElementById("root")
 );
